723428df6821f7b82937334a671c5e2a53daa61a
OppenSource <brice.nanyang@institutsaintjean.org>
2023-10-22 09:39:58 +0100

    finalisation des opérations de Crud sur les sous-catégories

--- src/main/java/com/example/intervenction/repositories/SousCategorieRepository.java
+++ src/main/java/com/example/intervenction/repositories/SousCategorieRepository.java
@@ -2,6 +2,8 @@ package com.example.intervenction.repositories;
 
 import com.example.intervenction.entities.SousCategorie;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface SousCategorieRepository extends JpaRepository<SousCategorie, Long> {
 }
--- src/main/java/com/example/intervenction/services/SousCategorieService.java
+++ src/main/java/com/example/intervenction/services/SousCategorieService.java
@@ -6,4 +6,15 @@ import com.example.intervenction.entities.SousCategorie;
 import java.util.List;
 
 public interface SousCategorieService {
+    List<SousCategorie> getAllSousCategories();
+
+    SousCategorie oneSousCategorie(Long id);
+
+    String addSousCategorie(SousCategorie sousCategorie, Long id_categorie);
+
+    String updateSousCategorie(Long id_categorie, Long id_sous_categorie, SousCategorie sousCategorie);
+
+    String enableSousCategorie(Long id);
+
+    String disableSousCategorie(Long id);
 }
